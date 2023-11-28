bdef9d0a4fab0b0c545a19764ac20ffdfec148bd
joseignacioo <romerojose5406@gmail.com>
2023-11-28 03:27:16 -0300

    commit

--- build.sh
+++ build.sh
@@ -5,7 +5,5 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py create_superuser --no-input
