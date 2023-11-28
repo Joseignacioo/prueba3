a086283aaecfffe502707d2b84ba96141547c9d2
joseignacioo <romerojose5406@gmail.com>
2023-11-28 03:07:15 -0300

    commit

--- prueba3/settings.py
+++ prueba3/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(" ")
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
