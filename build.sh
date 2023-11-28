db72e17aaf364c193a576006edc7704c6d1e0df0
joseignacioo <romerojose5406@gmail.com>
2023-11-28 04:05:36 -0300

    commit

--- build.sh
+++ build.sh
@@ -9,16 +9,16 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Nombre de usuario, correo electrónico y contraseña para el superusuario
-username="admin"
-email="admin@example.com"
-password="admin_password"
+# username="admin"
+# email="admin@example.com"
+# password="admin_password"
 
-# Verifica si el superusuario ya existe
-if ! python -c "from django.contrib.auth.models import User; print(User.objects.filter(username='$username').exists())"; then
-    # Crea un nuevo superusuario
-    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$username', '$email', '$password')" | python manage.py shell
+# # Verifica si el superusuario ya existe
+# if ! python -c "from django.contrib.auth.models import User; print(User.objects.filter(username='$username').exists())"; then
+#     # Crea un nuevo superusuario
+#     echo "from django.contrib.auth.models import User; User.objects.create_superuser('$username', '$email', '$password')" | python manage.py shell
 
-    echo "Superusuario creado con éxito!"
-else
-    echo "El superusuario ya existe."
-fi
+#     echo "Superusuario creado con éxito!"
+# else
+#     echo "El superusuario ya existe."
+# fi
